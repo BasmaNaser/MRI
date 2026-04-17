b6a5f710f9757bc32ee38435e99b92a6807b576a
BasmaNaser <bosynaser599@gmail.com>
2026-04-17 15:13:04 +0200

    Update doctorRoutes.js

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const doctorController = require('../controllers/doctorController');
-const { protect } = require('../middleware/authMiddleware');
+const doctorController = require('../Controllers/doctorController');
+const { protect } = require('../Middleware/authMiddleware');
 
 /**
  * @swagger
