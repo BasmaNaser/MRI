262ead8ca1afe530ff9c51013ee761fb4085e483
AhmedAmer404 <ahmedamerxx5@gmail.com>
2026-06-11 04:55:36 +0300

    Update static file serving for uploads directory

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use('/uploads', express.static('uploads'));
+const path = require('path');
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(logger);
 app.use(express.json());
 app.use(cookieParser());
